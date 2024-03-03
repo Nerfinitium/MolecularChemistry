7007102d1376fb2dea665ffa07d2c2795902bb06
Emir <buldukemir1@gmail.com>
2024-03-03 22:47:28 +0300

    Losing my mind baby losing my mind

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MolecularChemistry/'
+  base: '/molekuler-kimyam/', // Adjust to your actual repository name
 
 })
